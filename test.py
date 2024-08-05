63b39d277a48e82c2df63dafc7202af89910b6fa
Ji-yutong <1415888356@qq.com>
2024-08-06 06:21:00 +0800

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print("CUDA Available:", torch.cuda.is_available())  # 检查 CUDA 是否可用
-print("CUDA Version:", torch.version.cuda)  # 查看 PyTorch 支持的 CUDA 版本
-print("CUDA Device Count:", torch.cuda.device_count())  # 检查可用的 CUDA 设备数量
